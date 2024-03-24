78385c8b665c876591bd5472ffdfb47ed95b6dcf
RickSchanze <2815091564@qq.com>
2024-03-24 12:55:54 +0800

    feat(ThirdParty): 引入asio 主要为了协程

---
+++ Engine/Core/Coroutine/Coroutine.h
@@ -0,0 +1,18 @@
+/**
+ * @file Coroutine.h
+ * @author Echo 
+ * @Date 24-3-24
+ * @brief 
+ */
+
+#ifndef TEST_H
+#define TEST_H
+
+#include "asio.hpp"
+#include "asio/awaitable.hpp"
+#include "asio/co_spawn.hpp"
+#include "asio/this_coro.hpp"
+#include "asio/use_awaitable.hpp"
+#include "asio/detached.hpp"
+
+#endif //TEST_H
